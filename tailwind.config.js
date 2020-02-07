6c6440a3824d587ef7d03d4012bd59cee1d39a47
Omar Khatib <omar@omarkhatib.co>
2020-02-07 15:26:30 +0100

    add and test css logical properties plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+
 module.exports = {
+   corePlugins: {
+    padding: false,
+    margin: false,
+  },
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [
-    require('tailwind-css-variables')({},{postcssEachVariables:true})
+    require('tailwind-css-variables')({}, { postcssEachVariables: true }),
+    require('tailwind-css-logical-properties')({
+      logicalBorderRaduis:true
+    }),
   ],
 }
